108c5694c366597868f0a3ccafa8f56f20dd6351
danlagos <lagos.d@gmail.com>
2019-04-05 15:58:18 -0700

    add and delete button to list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+      { name: 'Daniel', age: "He looks too good to be this old", belt: "He's the MASTER!", id: 4}
+    ]
+  }
+
+// creates a random number assigned to the id property.  new variable that takes in the spread of this.state (original Ninjas Arr) and adds the new Ninja.
+//  created a new array.
+  addNinja = (ninja) => {
+    ninja.id = Math.random()
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> This is the body </h1>
+        <Ninjas ninjas={ this.state.ninjas } deleteNinja = {this.deleteNinja} />
+        <AddNinja addNinja = {this.addNinja} />
       </div>
     );
   }
